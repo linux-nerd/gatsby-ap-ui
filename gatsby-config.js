a00077e6218c6a13df9e721f831206dcf2697cfe
Abhishek Prakash <abhishekabby@gmail.com>
2020-09-13 23:33:00 +0200

    added changes for sentry

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,15 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/static/ap-icon.png` // This path is relative to the root of the site.
       }
+    },
+    {
+      resolve: "gatsby-plugin-sentry",
+      options: {
+        dsn: "https://2fdf0f2fd83947608f6363af508a3a4c@o447502.ingest.sentry.io/5427444",
+        // Optional settings, see https://docs.sentry.io/clients/node/config/#optional-settings
+        environment: process.env.NODE_ENV,
+        enabled: (() => ["production", "stage"].indexOf(process.env.NODE_ENV) !== -1)()
+      }
     }
   ]
 };
